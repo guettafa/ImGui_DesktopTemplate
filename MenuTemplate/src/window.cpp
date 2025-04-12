2fd98957dd0ed5f0a3c535db2d9970eaf4c7f266
guettafa <irontitou5@gmail.com>
2025-04-11 20:23:44 -0400

    Updated WndClass definition

--- MenuTemplate/src/window.cpp
+++ MenuTemplate/src/window.cpp
@@ -3,53 +3,69 @@
 
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wparam, LPARAM lparam)
 {
+	switch (msg)
+	{
+	case WM_CREATE:
+		// create window
+		return 0;
+	case WM_QUIT:
+		// clean & close
+		return 0;
+	default:
+		return DefWindowProc(hWnd, msg, wparam, lparam);
+	}
 	return 0;
 }
 
 VOID Window::CreateWnd()
 {
-	WNDCLASSEXW wndClass =
-	{
-		sizeof(wndClass),
-		CS_CLASSDC,
-		WndProc,
-		0L,
-		0L,
-		GetModuleHandle(nullptr),
-		nullptr,
-		nullptr,
-		nullptr,
-		nullptr,
-		L"MyWindow",
-		nullptr
-	};
+	WNDCLASSEX wndClass{};
+	wndClass.cbSize			= sizeof(wndClass);
+	wndClass.style			= CS_CLASSDC;
+	wndClass.lpfnWndProc	= WndProc;
+	wndClass.cbClsExtra		= 0;
+	wndClass.cbWndExtra		= 0;
+	wndClass.hInstance		= GetModuleHandle(nullptr);
+	wndClass.hIcon			= nullptr;
+	wndClass.hCursor		= nullptr;
+	wndClass.hbrBackground  = nullptr;
+	wndClass.lpszMenuName	= nullptr;
+	wndClass.lpszClassName	= L"MyWindow";
+	wndClass.hIconSm		= nullptr;
 
-	DWORD styles = WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_NOACTIVATE;
-	hWnd = CreateWindowEx(styles, L"MyWindow", nullptr, WS_POPUP, 0, 0, m_width, m_height, NULL, NULL, wndClass.hInstance, nullptr);
-
-	SetLayeredWindowAttributes(hWnd, 0, 0, ULW_COLORKEY);
-	
 	if (!RegisterClassEx(&wndClass))
 		std::printf("[-] Can't register WndClass %d", GetLastError());
+
+	DWORD windowStyles = WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_NOACTIVATE;
+	hWnd = CreateWindowEx(windowStyles, L"MyWindow", nullptr, WS_POPUP, 0, 0, m_width, m_height, NULL, NULL, wndClass.hInstance, nullptr);
+	
+	if (!hWnd)
+	{
+		std::printf("[-] HWND created by CreateWindowEx is NULL %d", GetLastError());
+		std::terminate();
+	}
+
+	SetLayeredWindowAttributes(hWnd, 0, 0, ULW_COLORKEY);
 }
 
 VOID Window::InitDevice()
 {
 	pD3d9 = Direct3DCreate9(D3D_SDK_VERSION);
 
-	if (!pD3d9) {
+	if (!pD3d9) 
+	{
 		std::printf("[-] Can't create the directx device %d \n", GetLastError());
 		std::terminate();
 	}
 
 	ZeroMemory(&m_d3dParameters, sizeof(m_d3dParameters));
-	m_d3dParameters.Windowed = TRUE;
-	m_d3dParameters.SwapEffect = D3DSWAPEFFECT_DISCARD;
-	m_d3dParameters.BackBufferFormat = D3DFMT_A8R8G8B8;  // Format 
-	m_d3dParameters.EnableAutoDepthStencil = TRUE;
-	m_d3dParameters.AutoDepthStencilFormat = D3DFMT_D16;
-	m_d3dParameters.PresentationInterval = D3DPRESENT_INTERVAL_ONE;
-	m_d3dParameters.MultiSampleType = D3DMULTISAMPLE_NONE;
+	m_d3dParameters.Windowed				= TRUE;
+	m_d3dParameters.SwapEffect				= D3DSWAPEFFECT_DISCARD;
+	m_d3dParameters.BackBufferFormat		= D3DFMT_A8R8G8B8;  // Format 
+	m_d3dParameters.EnableAutoDepthStencil	= TRUE;
+	m_d3dParameters.AutoDepthStencilFormat	= D3DFMT_D16;
+	m_d3dParameters.PresentationInterval	= D3DPRESENT_INTERVAL_ONE;
+	m_d3dParameters.MultiSampleType			= D3DMULTISAMPLE_NONE;
 
 	// Create the D3D device
 	HRESULT hr = pD3d9->CreateDevice(
@@ -62,7 +78,8 @@ VOID Window::InitDevice()
 	);
 
 	// Double check
-	if (FAILED(hr)) {
+	if (FAILED(hr)) 
+	{
 		hr = pD3d9->CreateDevice(
 			D3DADAPTER_DEFAULT,
 			D3DDEVTYPE_HAL,
@@ -87,16 +104,16 @@ VOID Window::InitImGui()
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 	io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
 
-	if (!ImGui_ImplWin32_Init(hWnd)) {
+	if (!ImGui_ImplWin32_Init(hWnd)) 
+	{
 		std::printf("[-] Can't initialize ImGui Win32 API %d \n", GetLastError());
 		std::terminate();
 	}
-
-	if (!ImGui_ImplDX9_Init(pD3d9Device)) {
-		std::printf("[-] Can't initialize ImGui Dx9 API %d \n", GetLastError());
+	if (!ImGui_ImplDX9_Init(pD3d9Device)) 
+	{
+		std::printf("[-] Can't initialize ImGui DirectX9 API %d \n", GetLastError());
 		std::terminate();
 	}
-
 	ImGui::StyleColorsDark();
 }
 
